30abf74b9090ed5f7fcdc82f6954698d93bf1c8b
Maura <mputzer@smith.edu>
2024-02-06 22:03:35 -0500

    Commit

--- computer.py
+++ computer.py
@@ -2,9 +2,38 @@ class Computer:
 
     # What attributes will it need?
 
+    year_made: int
+    new_os: Optional[str] = None
+    price: int
+    new_price: int
+
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
-    def __init__():
-        pass # You'll remove this when you fill out your constructor
+    def __init__(self):
+
+
+    def update_price(item_id: int, new_price: int):
+        if item_id in inventory:
+            inventory[item_id]["price"] = new_price
+        else:
+            print("Item", item_id, "not found. Cannot update price.")
+
+    def refurbish(item_id: int, new_os: Optional[str] = None):
+        if item_id in inventory:
+            computer = inventory[item_id] # locate the computer
+            if int(computer["year_made"]) < 2000:
+                computer["price"] = 0 # too old to sell, donation only
+            elif int(computer["year_made"]) < 2012:
+                computer["price"] = 250 # heavily-discounted price on machines 10+ years old
+            elif int(computer["year_made"]) < 2018:
+                computer["price"] = 550 # discounted price on machines 4-to-10 year old machines
+            else:
+                computer["price"] = 1000 # recent stuff
+
+            if new_os is not None:
+                computer["operating_system"] = new_os # update details after installing new OS
+        else:
+            print("Item", item_id, "not found. Please select another item to refurbish.")
+   
 
     # What methods will you need?
--- oo_resale_shop.py
+++ oo_resale_shop.py
@@ -2,6 +2,12 @@ class ResaleShop:
 
     # What attributes will it need?
 
+    item_id: int
+    computer: dict
+    new_price: int
+    price: int
+
+
     # How will you set up your constructor?
     # Remember: in python, all constructors have the same name (__init__)
     def __init__():
